8bf72ff13fa161d92b6ae6e831fcae1df5fb8b02
Kevin Paul Kalis <kevinpaulkalis@gmail.com>
2016-11-09 10:25:39 +0800

    Apply filter on text

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,1 @@
-angular.module("ngswift", [])
-    .controller("NgSwiftController", function ($scope) {
-        $scope.visible = true;
-
-        $scope.toggle = function() {
-            $scope.visible = !$scope.visible;
-        };
-    })
+angular.module("ngswift", []);
